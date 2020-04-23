a90d3e9f0c9d294196ed057b88a612e8b6cd2ede
Omar Nabi <onabi20@gmail.com>
2020-04-23 05:36:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(string)
+    @knowledge.push(string)
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
