a2a3064d069f585a490856662a07490b1520b7b2
Omar Nabi <onabi20@gmail.com>
2020-04-23 05:42:30 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(string)
+    knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
